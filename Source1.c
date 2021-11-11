f8f8f39984bf447e495bc8363f2073e460a7074b
judyela <87123889+judyela@users.noreply.github.com>
2021-11-11 14:38:46 +0200

    Ver2

--- Source1.c
+++ Source1.c
@@ -5,9 +5,6 @@ int main()
 	printf("Hello");
 	printf("Hello Sharon");
 	printf("our project");
-<<<<<<< HEAD
 	printf("Judy");
-=======
 	printf("our project2");
->>>>>>> 66dcc612c35ca05221275c8a01d5960273505710
 }
